a9bbf729edcb97452269341abd76f73929d9f7b7
netwarp <resethread@gmail.com>
2023-04-16 17:09:03 +0200

    add public methods

--- src/seven-seas.js
+++ src/seven-seas.js
@@ -6,9 +6,70 @@ class SevenSeas {
      * @param {string} secret - API secret
      */
     constructor(key, secret) {
+        this.ssx_url = 'https://www.sevenseas.exchange/api/v1';
+
         this.key = key;
         this.secret = secret;
     }
 
+    /**
+     *
+     * @returns {Promise<any>}
+     */
+    async fetchAllMarkets() {
+        try {
+            const response = await fetch(`${this.ssx_url}/markets`);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching market data:", error);
+        }
+    }
 
+    /**
+     *
+     * @param {string} market
+     * @returns {Promise<void>}
+     */
+    async fetchOneMarket(market) {
+        try {
+            const response = await fetch(`${this.ssx_url}/markets/${market}`);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching market data:", error);
+        }
+    }
+
+    /**
+     *
+     * @param {string} market -- example BTC-USDT
+     * @returns {Promise<void>}
+     */
+    async tradeHistory(market) {
+        try {
+            const response = await fetch(`${this.ssx_url}/history/${market}`);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching market data:", error);
+        }
+    }
+
+    /**
+     *
+     * @param {string} market
+     * @returns {Promise<void>}
+     */
+    async ordersBook(market) {
+        try {
+            const response = await fetch(`${this.ssx_url}/orders/${market}`);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching market data:", error);
+        }
+    }
 }
+
+export default SevenSeas
---
+++ test/lib.js
@@ -0,0 +1,41 @@
+import test from 'node:test';
+import * as assert from "assert";
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import SevenSeas from '../src/seven-seas.js'
+
+const sevenSeas = new SevenSeas(process.env.KEY, process.env.SECRET)
+
+
+test('fetch all markets', {skip: true}, async (t) => {
+
+    const markets = await sevenSeas.fetchAllMarkets();
+
+    assert.strictEqual(markets.length > 10, markets.length > 10);
+});
+
+test('fetch one market - BTC-XMR', {skip: true}, async (t) => {
+
+    const market = await sevenSeas.fetchOneMarket('xmr-btc');
+
+    assert.strictEqual(market['symbol'], market['symbol'])
+});
+
+test('fetch history of market', {skip: true}, async (t) => {
+
+    const history = await sevenSeas.tradeHistory('xmr-btc');
+
+    assert.strictEqual(history.length > 4, history.length > 4)
+});
+
+test('fetch orders book of market', {skip: false}, async (t) => {
+
+    const history = await sevenSeas.ordersBook('xmr-btc');
+
+    assert.strictEqual(history.length > 4, history.length > 4)
+});
+
+
+
