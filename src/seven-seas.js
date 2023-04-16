4bdb9b732996b3cd28e8ab0960b28219338df30a
netwarp <resethread@gmail.com>
2023-04-16 15:54:45 +0200

    start lib

---
+++ src/seven-seas.js
@@ -0,0 +1,14 @@
+class SevenSeas {
+
+    /**
+     *
+     * @param {string} key - API secret
+     * @param {string} secret - API secret
+     */
+    constructor(key, secret) {
+        this.key = key;
+        this.secret = secret;
+    }
+
+
+}
